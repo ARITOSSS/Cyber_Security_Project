2543e28b8ef10518cffb3b62b52c66d4957079c0
Aristide <aridu17@gmail.com>
2024-11-15 14:19:17 +0200

    First Flaw

--- security_flaws/urls.py
+++ security_flaws/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('xss_flaw/', views.xss_flaw, name='xss_flaw'),
+    path('xss-fix/', views.xss_fix, name='xss_fix'),
+
 ]
--- security_flaws/views.py
+++ security_flaws/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'security_flaws/home.html')
+
+def xss_flaw(request):
+    user_input = request.GET.get('user_input','')
+    return render(request, 'security_flaws/xss_flaw.html' , {'user_input': user_input})
+
+def xss_fix(request):
+    user_input = request.GET.get('user_input','')
+    return render(request, 'security_flaws/xss_fix.html' , {'user_input': user_input})
