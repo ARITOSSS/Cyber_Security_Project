b44a6638cd8fc6097a817c12cdc600bda306bcea
Aristide <aridu17@gmail.com>
2024-11-19 16:47:48 +0200

    second flaw

--- security_flaws/urls.py
+++ security_flaws/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('xss_flaw/', views.xss_flaw, name='xss_flaw'),
     path('xss-fix/', views.xss_fix, name='xss_fix'),
+    path('csrf_flaw/', views.csrf_flaw, name='csrf_flaw'),
+    path('csrf_fix/', views.csrf_fix, name='csrf_fix'),
 
 ]
--- security_flaws/views.py
+++ security_flaws/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -13,3 +14,15 @@ def xss_flaw(request):
 def xss_fix(request):
     user_input = request.GET.get('user_input','')
     return render(request, 'security_flaws/xss_fix.html' , {'user_input': user_input})
+
+def csrf_flaw(request):
+    if request.method == 'POST':
+        user_data = request.POST.get('user_data', '')
+        return HttpResponse(f"Données reçues : {user_data}")
+    return render(request, 'security_flaws/csrf_flaw.html')
+
+def csrf_fix(request):
+    if request.method == 'POST':
+        user_data = request.POST.get('user_data', '')
+        return HttpResponse(f"Données reçues : {user_data}")
+    return render(request, 'security_flaws/csrf_fix.html')
